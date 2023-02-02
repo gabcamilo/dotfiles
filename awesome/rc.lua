598dce93d60da510a0a20ef2327e6293227ed6dd
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2023-02-01 22:07:12 -0300

    [awesome] adding SUPER + Tab window navigation

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -268,6 +268,12 @@ globalkeys = gears.table.join(
         end,
         {description = "focus previous by index", group = "client"}
     ),
+
+    awful.key({ modkey,         }, "Tab", function () awful.client.focus.byidx(  1) end,
+        {description = "focus previous by index", group = "client"}),
+    awful.key({ modkey, "Shift" }, "Tab", function () awful.client.focus.byidx( -1) end,
+        {description = "focus next by index", group = "client"}),
+
     awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
 
@@ -282,14 +288,14 @@ globalkeys = gears.table.join(
               {description = "focus the previous screen", group = "screen"}),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
-    awful.key({ modkey,           }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end,
-        {description = "go back", group = "client"}),
+    -- awful.key({ modkey,           }, "Tab",
+    --     function ()
+    --         awful.client.focus.history.previous()
+    --         if client.focus then
+    --             client.focus:raise()
+    --         end
+    --     end,
+    --     {description = "go back", group = "client"}),
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
